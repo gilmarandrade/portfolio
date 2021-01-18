e77299414c2e48c47f03a8185f426453b993011b
Gilmar Andrade <gilmar-andrade@outlook.com>
2021-01-18 10:07:31 -0300

    index

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.sendFile(path.join(__dirname, "index.html"));
 })
                                 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
+    console.log('[gilmarandrade.com] listening on port %s', port);
 });
